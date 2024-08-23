dcd745f83bdc825f89fe69cd2eda20f8b955210c
ptsdpatient <119649887+ptsdpatient@users.noreply.github.com>
2024-08-23 21:39:49 +0530

    Update login_page.dart

--- app/lib/pages/login_page.dart
+++ app/lib/pages/login_page.dart
@@ -12,48 +12,86 @@ class _LoginPage extends State<LoginPage>{
     // TODO: implement build
     return Scaffold(
         body:Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset("images/icon.jpg",width:200),
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset("images/icon.jpg",width:200),
                     SizedBox(height:15),
                     Text("s a f e g u a r d", style:TextStyle(color:Colors.orange,fontSize: 20)),
                     Text("Mobile Service", style:TextStyle(color:Colors.orange,fontSize: 10)),
-                ],
-            ),
-              Column(
-                children: [
-                  Padding(padding: EdgeInsets.symmetric(vertical: 10),
-                    child:Text("Login",style : TextStyle(
-                      fontSize: 25,
-                    )),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25),
-                      boxShadow: BoxShadow(Colors.grey,)
+                    SizedBox(height:30)
+                  ],
+                ),
+                Column(
+                  children: [
+                    Padding(padding: EdgeInsets.symmetric(vertical: 20),
+                      child:Text("Login",style : TextStyle(
+                        fontSize: 25,
+                      )),
                     ),
-                    child: ClipRRect(
-                      child: Padding(padding: EdgeInsets.symmetric(horizontal: 30),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            label: Text("Username/ Email"),
-                            prefix: Padding(padding: EdgeInsets.fromLTRB(0, 4, 10, 0),child:Icon(Icons.account_circle,size:34)),
+                    Padding(padding: EdgeInsets.symmetric(horizontal: 25,vertical: 10),
+                        child:Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white,
+                              boxShadow: const [
+                                BoxShadow(
+                                  color:Colors.black26,
+                                  offset:Offset(0.5, 0.5),
+                                  spreadRadius: 0.5,
+                                  blurRadius: 0.5,
+                                )
+                              ]
                           ),
-                        ),
-                      ),
+                          child: const TextField(
+                              cursorColor: Colors.pink,
+                              decoration:InputDecoration(
+                                  icon:Padding(padding: EdgeInsets.symmetric(horizontal: 5),child: Icon(Icons.account_circle,size:35,color:Colors.pink),),
+                                  border:InputBorder.none ,
+                                  labelText: "username/email",
+                                  labelStyle:TextStyle(color: Colors.black54)
+                              )),
+                        )
                     ),
-                  )
+                    Padding(padding: EdgeInsets.symmetric(horizontal: 25,vertical: 10),
+                        child:Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white,
+                              boxShadow: const [
+                                BoxShadow(
+                                  color:Colors.black26,
+                                  offset:Offset(0.5, 0.5),
+                                  spreadRadius: 0.5,
+                                  blurRadius: 0.5,
+                                )
+                              ]
+                          ),
+                          child: const TextField(
+                              cursorColor: Colors.pink,
+                              decoration:InputDecoration(
+                                  icon:Padding(padding: EdgeInsets.symmetric(horizontal: 5),child: Icon(Icons.password_rounded,size:35,color:Colors.pink),),
+                                  border:InputBorder.none ,
+                                  labelText: "password",
+                                  labelStyle:TextStyle(color: Colors.black54)
+                              )
+                          ),
+                        )
+                    ),
 
-                ],
-              )
-          ],
-        ),
+                  ],
+                )
+              ],
+            ),
+          )
       ),
     );
   }
